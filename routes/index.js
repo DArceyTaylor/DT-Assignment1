7cf50aa977fbf8e442184482c8d53a65d6fc5866
D'Arcey Taylor <darceytaylor05@gmail.com>
2016-02-05 16:08:23 -0500

    added in template & bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
