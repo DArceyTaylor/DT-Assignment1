af42da49c1b1d312599aab4a0de91aabbd8341e2
D'Arcey Taylor <darceytaylor05@gmail.com>
2016-02-10 18:11:42 -0500

    updated index.js

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Portfolio' });
 });
 /* GET About Me page. */
 router.get('/#about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('index', { title: 'Portfolio' });
 });
 /* GET Projects page. */
 router.get('/#projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('index', { title: 'Portfolio' });
 });
 /* GET Contact Me page. */
 router.get('/#contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
+  res.render('contact', { title: 'Portfolio' });
 });
 
 module.exports = router;
