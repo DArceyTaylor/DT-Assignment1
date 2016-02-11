681c8e68c38cad94aaf4039950f0a04e24f69dfe
D'Arcey Taylor <darceytaylor05@gmail.com>
2016-02-10 20:24:44 -0500

    added comment headers

--- public/Scripts/main.js
+++ public/Scripts/main.js
@@ -3,7 +3,7 @@
     Author: D'Arcey Taylor
     File: DTASSIGNMENT1
     Website: http://dt-assignment1.azurewebsites.net/#home
-    Reason: This allows for special font characters
+    Reason: This is the prime javascript
 */
 /*
 	Prologue by HTML5 UP
